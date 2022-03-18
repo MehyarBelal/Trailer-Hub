30dcf9917d4276da63bdb6f6bd1afbd8c475a40e
s3559505 <53518501+s3559505@users.noreply.github.com>
2022-03-18 20:32:31 +1100

    m

--- trailerhub/src/components/Sidebar.js
+++ trailerhub/src/components/Sidebar.js
@@ -1,15 +1,16 @@
 import React from 'react'
 
+
 function Sidebar() {
   return (
     <div className='sidebar'>
 
-<div className='logo'><h1>Trailer Hub</h1></div>
+<div className='logo'><h1>Trailer Hub <i class="fa-solid fa-clapperboard"></i></h1></div>
 <ul>
 
   <li>Trending</li>
-  <li>Trending</li>
-  <li>Trending</li>
+  <li>Top Rated</li>
+  <li>New Releases</li>
 
 </ul>
         
